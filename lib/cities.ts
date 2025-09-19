caff247ea4de3c7ea63dcd72190e3e35ce50eb65
hmzabrd <hamzabordo999@gmail.com>
2025-09-19 22:33:08 +0100

    Add files via upload

---
+++ lib/cities.ts
@@ -0,0 +1,21 @@
+export const cities = {
+	type: "FeatureCollection",
+	features: [
+		{ type: "Feature", properties: { id: "casablanca", fr: "Casablanca", ar: "الدار البيضاء", venue: "Jardin de l'Université Arabe", time: "18:00", date_fr: "27/28 septembre 2025", date_ar: "27/28 شتنبر 2025", icon: "🕌" }, geometry: { type: "Point", coordinates: [-7.617, 33.594] } },
+		{ type: "Feature", properties: { id: "rabat", fr: "Rabat", ar: "الرباط", venue: "À côté du Parlement", time: "18:00", date_fr: "27/28 septembre 2025", date_ar: "27/28 شتنبر 2025", icon: "🏛️" }, geometry: { type: "Point", coordinates: [-6.833, 34.02] } },
+		{ type: "Feature", properties: { id: "marrakech", fr: "Marrakech", ar: "مراكش", venue: "Eden O'Tacos / McDo (Carré Eden)", time: "18:00", date_fr: "27/28 septembre 2025", date_ar: "27/28 شتنبر 2025", icon: "🎭" }, geometry: { type: "Point", coordinates: [-8.0086, 31.6325] } },
+		{ type: "Feature", properties: { id: "fes", fr: "Fès", ar: "فاس", venue: "Bab Boujloud", time: "18:00", date_fr: "27/28 septembre 2025", date_ar: "27/28 شتنبر 2025", icon: "📚" }, geometry: { type: "Point", coordinates: [-4.983, 34.065] } },
+		{ type: "Feature", properties: { id: "meknes", fr: "Meknès", ar: "مكناس", venue: "McDonald's Hamria", time: "18:00", date_fr: "27/28 septembre 2025", date_ar: "27/28 شتنبر 2025", icon: "🏰" }, geometry: { type: "Point", coordinates: [-5.54, 33.893] } },
+		{ type: "Feature", properties: { id: "eljadida", fr: "El Jadida", ar: "الجديدة", venue: "Hôtel Ibis", time: "18:00", date_fr: "27/28 septembre 2025", date_ar: "27/28 شتنبر 2025", icon: "🌊" }, geometry: { type: "Point", coordinates: [-8.504, 33.247] } },
+		{ type: "Feature", properties: { id: "agadir", fr: "Agadir", ar: "أكادير", venue: "Rue de l'Armée Royale", time: "18:00", date_fr: "27/28 septembre 2025", date_ar: "27/28 شتنبر 2025", icon: "🏖️" }, geometry: { type: "Point", coordinates: [-9.597, 30.427] } },
+		{ type: "Feature", properties: { id: "oujda", fr: "Oujda", ar: "وجدة", venue: "Avenue Mohammed V", time: "18:00", date_fr: "27/28 septembre 2025", date_ar: "27/28 شتنبر 2025", icon: "🧭" }, geometry: { type: "Point", coordinates: [-1.911, 34.681] } },
+		{ type: "Feature", properties: { id: "tetouan", fr: "Tétouan", ar: "تطوان", venue: "Place Moulay el Mehdi", time: "18:00", date_fr: "27/28 septembre 2025", date_ar: "27/28 شتنبر 2025", icon: "🏔️" }, geometry: { type: "Point", coordinates: [-5.367, 35.572] } },
+		{ type: "Feature", properties: { id: "tanger", fr: "Tanger", ar: "طنجة", venue: "Place des Nations", time: "18:00", date_fr: "27/28 septembre 2025", date_ar: "27/28 شتنبر 2025", icon: "🚢" }, geometry: { type: "Point", coordinates: [-5.806, 35.776] } },
+		{ type: "Feature", properties: { id: "nador", fr: "Nador", ar: "الناظور", venue: "Place Chabiba", time: "18:00", date_fr: "27/28 septembre 2025", date_ar: "27/28 شتنبر 2025", icon: "🌅" }, geometry: { type: "Point", coordinates: [-2.927, 35.169] } },
+	],
+} as const
+
+export const protestDates = ["2025-09-27T18:00:00+01:00", "2025-09-28T18:00:00+01:00"]
+
+
+
